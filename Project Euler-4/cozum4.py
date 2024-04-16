5dc845087207bc31a425b6187444826a0ce8e151
python533 <143389231+python533@users.noreply.github.com>
2024-04-16 20:43:19 +0300

    Project Euler-4
    Soru: Palindrom sayılar her iki yönden de aynı okunan sayılardır. İki basamaklı iki sayının çarpımıyla oluşturulabilen en büyük palindrom 91*99=9009'dur. Üç basamaklı iki sayının çarpımı olarak yazılabilen en büyük palindromu bulunuz.

---
+++ Project Euler-4/cozum4.py
@@ -0,0 +1,14 @@
+list=[]
+
+def polin(i):
+    for i in range(101,1000):
+        if i %11 ==0:
+            ctr=999
+            son_liste=list()
+
+    while ctr >=1000:
+        for i in a:
+            if str(ctr*i) == str(ctr * i) [::-1]:
+                list.append(ctr*i)
+        ctr -=1
+        print(max(list))
